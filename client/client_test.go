ae683c58868b5a8c2450626f81fadd224e9adcd9
h15z <h15z>
2022-01-16 13:23:32 +0100

    restclient tests added

--- client/client_test.go
+++ client/client_test.go
@@ -16,8 +16,6 @@ var testing_accounts_version int
 func TestCreateClient(t *testing.T) {
 	c := New()
 	assert.NotEmpty(t, c.Accounts.Client)
-
-	//TODO add more assetions here
 }
 
 //End-to-end accounts tests
@@ -27,30 +25,6 @@ func TestAccountsCreateSuccess(t *testing.T) {
 	ctx, cancel := context.WithTimeout(ctx, 15*time.Second)
 	defer cancel()
 	c := New()
-
-	/*
-		REFERENCE DATA
-
-		{
-		"data": {
-			"id": "771d850a-f6b3-4c16-b544-bbc8a05b740d",
-			"organisation_id": "e2e47bf9-1d1a-46fc-930d-92a43903f857",
-			"type": "accounts",
-			"attributes": {
-				"country": "GB",
-				"base_currency": "GBP",
-				"bank_id": "400302",
-				"bank_id_code": "GBDSC",
-				"account_number": "10000004",
-				"customer_id": "234",
-				"iban": "GB28NWBK40030212764204",
-				"bic": "NWBKGB42",
-				"account_classification": "Personal",
-				"name" : ["name"]
-			}
-		}
-
-	*/
 
 	//Declare Payload Struct
 	country := "GB"
@@ -69,17 +43,14 @@ func TestAccountsCreateSuccess(t *testing.T) {
 			Bic:                   "NWBKGB42",
 			AccountClassification: &account_classification,
 			Name:                  []string{"Name", "Surname"},
-			// CustomerId:    "234",
 		},
 	}
 
 	guid, resp, err := c.Accounts.Create(ctx, payload)
 	assert.Equal(t, nil, err)
-	// assert.Equal(t, "/v1/organisation/accounts/"+guid, resource_link)
 	assert.True(t, strings.Contains(resp.Links.Self, "/v1/organisation/accounts/"))
 	assert.NotEmpty(t, resp.Data)
 	assert.NotEqual(t, "", guid)
-	// assert.True(t, IsValidUUID(guid))
 
 	testing_accounts_resource_link = resp.Links.Self
 	testing_accounts_version = int(*resp.Data.Version)
@@ -109,7 +80,6 @@ func TestAccountsCreateFailWithError(t *testing.T) {
 			AccountClassification: &account_classification,
 			// Iban:                  "GB28NWBK40030212764204",
 			// Name:                  []string{"Name", "Surname"},
-			// CustomerId:    "234",
 		},
 	}
 
@@ -174,5 +144,4 @@ func TestAccountsDeleteSuccess(t *testing.T) {
 
 	assert.Equal(t, nil, err)
 	assert.Equal(t, "204\tNo Content\tResource has been successfully deleted", resp)
-
 }
--- client/restclient/restclient_test.go
+++ client/restclient/restclient_test.go
@@ -1,7 +1,70 @@
 package restclient
 
-// test get
+import (
+	"context"
+	"fmt"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/H15Z/-interview-accountapi/client/models"
+	"github.com/stretchr/testify/assert"
+)
 
 // test post
+func TestRestPOST(t *testing.T) {
+
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, 15*time.Second)
+	defer cancel()
+	c := NewClient(Defaults())
+
+	// 404 not found
+	resp, err := c.PostRequest(ctx, "", models.AccountData{})
+	assert.Equal(t, "Page not found", err.Error())
+	assert.Equal(t, `{"code":"PAGE_NOT_FOUND","message":"Page not found"}`, string(resp))
+
+	// valid url
+	_, err = c.PostRequest(ctx, "/v1/organisation/accounts", models.AccountData{})
+	assert.Equal(t, "validation failure list:\nvalidation failure list:\nattributes in body is required\nid in body is required\norganisation_id in body is required\ntype in body is required", err.Error())
+
+}
+
+// test get
+func TestRestGET(t *testing.T) {
+
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, 15*time.Second)
+	defer cancel()
+	c := NewClient(Defaults())
+
+	// 404 not found
+	resp, err := c.GetRequest(ctx, "")
+	assert.Equal(t, "Page not found", err.Error())
+	assert.Equal(t, `{"code":"PAGE_NOT_FOUND","message":"Page not found"}`, string(resp))
+
+	resp, err = c.GetRequest(ctx, "/v1/organisation/accounts")
+	assert.Equal(t, nil, err)
+	assert.True(t, strings.Contains(string(resp), `{"data":[{"attributes":{"`))
+
+}
 
 // test delete
+func TestRestDELETE(t *testing.T) {
+
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, 15*time.Second)
+	defer cancel()
+	c := NewClient(Defaults())
+
+	// 404 not found
+	resp, err := c.DeleteRequest(ctx, "")
+	assert.Equal(t, "delete failed with status code: 404 Not Found", err.Error())
+	assert.Equal(t, resp, 404)
+
+	// 400
+	resp, err = c.DeleteRequest(ctx, "/v1/organisation/accounts/771d850a-f6b3-4c16-b544-bbc8a05b740d")
+	assert.Equal(t, resp, 400)
+
+	fmt.Println(resp, err)
+}
